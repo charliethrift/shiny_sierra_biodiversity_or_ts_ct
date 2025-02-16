46fad64ca2eded628650f631aea3b1d1f59b393b
tanvijshah01 <tanvijshah01@gmail.com>
2025-02-16 00:39:02 -0800

    Tanvi's updates Slider bar for Species distribution Reactive plot for species distribution Dataset for species distribution

--- shiny_sierra_biodiversity_or_ts_ct/app.R
+++ shiny_sierra_biodiversity_or_ts_ct/app.R
@@ -1,51 +1,68 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# Shiny App for Species Distribution and Wildfire Events 
+# 2025 / 02 / 15
+
 
 library(shiny)
+library(here)
+library(dplyr)
+library(maps)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+# Data -----------------------------------------------------------------------
+#### Species Distribution Data - only Plants in Mono County
+species_cols <- c('year','month', 'day', 'phylum','class','order','family',
+                  'scientific_name','decimal_latitude', 'decimal_longitude' )
+species_df_all_years <- read_csv(here("shiny_sierra_biodiversity_or_ts_ct",
+                            "data", "occurrences.csv")) %>% clean_names() %>%
+  select(species_cols) %>% drop_na()
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+# Wrappers ------------------------------------------------------------------
+## to avoid cluttering UI and Server  
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+#### CA Species Distribution Wrapper
+species_dist_plot_year <- function(input_year){
+  data <- species_df_all_years %>% filter(year == input_year)
+  CA_counties <- map_data("county", region = "california")
+  ggplot(CA_counties, aes(x = long, y = lat, group = subregion)) +
+    geom_polygon(color="white") +
+    coord_map()+
+    geom_point(data = data, aes(x= decimal_longitude, y= decimal_latitude),
+               color = 'red',size = 1,
+               inherit.aes = FALSE )+
+    coord_equal()
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+# Wildfire Map Placeholder
+##### this data is static for now
+wildfire_plot_year <- function(input_year){
+  # input_year is a dummy variable for
+  CAcounties <- map_data("county", region = "california")
+  CA_wildfire_map <- ggplot(CAcounties, aes(x = long, y = lat, group = subregion)) +
+    geom_polygon(color="white") +
+    coord_map()
+}
+
+
+# UI --------------------------------------------------------------------------
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel("Choose Inputs",
+                 sliderInput("yr", "Year of Recorded Observation:",
+                             min = 1980, max = 2025, value = 2000)),
+    mainPanel("Map of Species Distribution",
+              plotOutput("species_dist_plot"))
+  )
+)
+
+# Server ----------------------------------------------------------------------
+server <- function(input, output) {
+  output$species_dist_plot <- renderPlot({
+    species_dist_plot_year(input$yr)
+  })
+}
+
+# Complete app with UI and server components
+shinyApp(ui, server)
+
+
+
+
