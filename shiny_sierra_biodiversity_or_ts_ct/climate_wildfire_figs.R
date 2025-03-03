5df5d376dcb6e94cf31b4810e3109cf345ecb34c
Olivia Ross <olross@ucsb.edu>
2025-03-02 16:07:00 -0800

    olivia updates

--- shiny_sierra_biodiversity_or_ts_ct/climate_wildfire_figs.R
+++ shiny_sierra_biodiversity_or_ts_ct/climate_wildfire_figs.R
@@ -22,6 +22,9 @@ get_prism_annual("ppt", years = 2000:2023, keepZip = FALSE)
 # SNARL coordinates 
 snarl <- c(-118.83317, 37.61404)
 
+# SNARL reserve boundary
+snarl_poly <- st_read(here("data/SNARL", "SNARL_boundary.shp"))
+
 # Plot 1: Maximum Annual Temperature at SNARL----------------------------------#
 
 # Grabbing our climate data from the directory
@@ -40,7 +43,7 @@ tmax_snarl +
   geom_line(color="coral2", lwd=1.5) +
   geom_point(alpha = 0.5)+
   labs(x = "Year") +
-  theme_minimal()
+  theme_bw()
 
 # Plot 2: Annual Precipitation at SNARL----------------------------------------#
 
@@ -60,7 +63,7 @@ ppt_snarl +
   geom_line(color="deepskyblue", lwd=1.5) +
   geom_point(alpha = 0.5)+
   labs(x = "Year") +
-  theme_minimal()
+  theme_bw()
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
 # Interactive wildfire map
@@ -97,22 +100,60 @@ snarl_df <- cbind(x, y)
 snarl_df <- as.data.frame(snarl_df)
 snarl_sf <- st_as_sf(snarl_df, coords = c("x", "y"), crs = 4326)
 
+# Creating a time column and time series for our fire data
+fire_snv$Year_numeric <- as.numeric(as.character(fire_snv$Year)) 
+
+fire_time <- ts(fire_snv$Year_numeric, start = 2000, frequency = 1)
+
 # Interactive Wildfire Map-----------------------------------------------------#
 tmap_mode("view")
+install.packages("gifski")
 
 tm_shape(snv) +
   tm_borders("black", lwd=1.0) +
   
-tm_shape(fire_snv) +
+  tm_shape(fire_snv) +
   tmap_options(check.and.fix = TRUE)+
   tm_fill(col = "Year", palette = "PiYG", title="Fire Year") +
   #tm_borders("black") +
   
-tm_shape(snarl_sf) +
-  tm_dots(col = "orange")+ #symbol type not supported in view mode
+  #tm_facets(by ="Year_numeric") + 
+  
+  tm_shape(snarl_poly) +
+  tm_dots(col = "orange") + #symbol type not supported in view mode
+  
+  tm_add_legend(type="fill", label = "SNARL", col = "orange") +
+  tm_add_legend(type="fill", label = "Sierra Nevada", col = "black")
 
-tm_add_legend(type="fill", label = "SNARL", col = "orange") +
-tm_add_legend(type="fill", label = "Sierra Nevada", col = "black")
+tmap_animation(fire_map, time = "Year_numeric", width = 800, height = 600)  
+
+fire_map
+
+# Map of SNARL-----------------------------------------------------------------#
+tmap_mode("view")
+
+tm_shape(snarl_poly) +
+  tm_polygons(col = "orange") 
+
+# Map of SNARL & Plants--------------------------------------------------------#
+
+# Reading in cal flora data
+snarl_calflora <- st_read(here("data/snarl_calflora", "shz1198.shp"))
+
+# Getting just the year 
+snarl_calflora <- snarl_calflora |>
+  mutate(Year = year(Date))
+
+# Making this a factor for the map
+snarl_calflora$Year <- as.factor(snarl_calflora$Year)
+
+tmap_mode("view")
+
+tm_shape(snarl_poly) +
+  tm_polygons(col = "orange") +
+  
+  tm_shape(snarl_calflora) +
+  tm_dots(col = "Year", palette = "PiYG", title="Observation Year")
 
 
 
